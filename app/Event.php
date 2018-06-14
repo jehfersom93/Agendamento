b8d2e18db624435d2e7439e1efe8541bd860d22d
jehfersom93 <jehfersom@gmail.com>
2018-06-14 00:46:38 -0300

    Adicionando o plugin fullcalendar para o projeto.

---
+++ app/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Agendamento;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /**
+    *[$table description]
+    *@var string
+    */
+    protected $table = 'events';
+
+    /**
+     *[$fillable description]
+     *@var string
+     */
+    protected  $fillable = [
+        'title', 'start', 'end', 'color'
+    ];
+}
---
+++ database/seeds/EventsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Agendamento\Event::class)
+            ->times(100)
+            ->create();
+    }
+}
